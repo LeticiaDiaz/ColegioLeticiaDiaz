778d5d329e60b81b5dcac598ddb21e385c8c974f
LeticiaDiaz <leticia.diaz.carmona@gmail.com>
2021-01-10 01:04:40 +0100

    cambiosuno

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Login";
+
+import { useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Route exact path="/registro/alumno">
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
--- servidor/index.js
+++ servidor/index.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const MongoClient = require("mongodb").MongoClient
-const cors = require("cors")
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const passport = require("passport");
 
 const app = express();
 const config = require("./config.json");
@@ -19,3 +20,116 @@ MongoClient.connect(config.mongopath, function (err, client) {
   }
 });
 
+app.post(
+  "/api/login",
+  passport.authenticate("local", {
+    successRedirect: "/api",
+    failureRedirect: "/api/fail",
+  })
+);
+
+app.get("/api/fail", function (req, res) {
+  res.status(401).send({ mensaje: "denegado" });
+});
+
+app.get("/api", function (req, res) {
+  if (req.isAuthenticated() === false) {
+    return res.status(401).send({ mensaje: "necesitas loguearte" });
+  }
+  res.send({ mensaje: "logueado correctamente" });
+});
+
+app.post("/registro/alumno", function (req, res) {
+  db.collection("alumnos").insertOne(
+    {
+      email: req.body.email,
+      password: req.body.password,
+    },
+    function (err, datos) {
+      if (err !== null) {
+        res.send(err);
+      } else {
+        res.send({ mensaje: "Registrado" });
+      }
+    }
+  );
+});
+
+const session = require("express-session");
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+const LocalStrategy = require("passport-local").Strategy;
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    function (email, password, done) {
+      db.collection("alumnos")
+        .find({ email: email })
+        .toArray(function (err, users) {
+          if (users.length === 0) {
+            done(null, false);
+          }
+          const user = users[0];
+          if (password === user.password) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+    }
+  )
+);
+
+new LocalStrategy(
+  {
+    usernameField: "email",
+  },
+  function (email, password, done) {
+    db.collection("alumnos")
+      .find({ email: email })
+      .toArray(function (err, users) {
+        if (users.length === 0) {
+          done(null, false);
+        }
+        const user = users[0];
+        if (password === user.password) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+  }
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function (id, done) {
+  db.collection("alumnos")
+    .find({ email: id })
+    .toArray(function (err, users) {
+      if (users.length === 0) {
+        done(null, null);
+      }
+      done(null, users[0]);
+    });
+});
+
+app.get("/api/user", function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.send({ nombre: req.user.name });
+  }
+  res.send({ nombre: "No logueado" });
+});
+
+app.listen(3001);
